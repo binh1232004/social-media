2f9f4b3db93065205b18a4ffd8cffb3ea591b5a8
TuanAnh <lngtuananh09@gmail.com>
2024-10-21 19:26:37 +0700

    add sidebar messenger

--- frontend/app/home/page.js
+++ frontend/app/home/page.js
@@ -1,12 +1,16 @@
 'use client';
 import React from "react";
 import NavBar from "../public/navbar";
+import MessageBar from "../public/messageBar";
 
 export default function HomePage() {
   return (
-    <div className="">
-      <div>
+    <div>
+      <div className="fixed">
         <NavBar/>
+      </div>
+      <div>
+        <MessageBar/>
       </div>
       <div className="mt-24 mx-6">
         <h1 className="text-white">Đang bảo trì</h1>
